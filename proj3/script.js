addbbaf0914be04ff48cbf5ade203ba325ebe72b
Jay Panchal <panchalj272@gmail.com>
2021-05-01 12:41:14 +0530

    sort method on array

--- proj3/script.js
+++ proj3/script.js
@@ -26,7 +26,7 @@ const courses = [{
             name: "Complete Django course",
             price: "7.0",
         },
-    ] // array of objct
+    ] // array of objects
 
 function generateList() {
     const ul = document.querySelector(".list-group");
@@ -73,3 +73,70 @@ button2.addEventListener("click", () => {
     courses.sort((a, b) => b.price - a.price); // this is in built sort method 
     generateList();
 });
+
+const button3 = document.querySelector(".sort-btn3");
+button3.addEventListener('click', () => {
+    courses.sort((a, b) => {
+        if (a.name < b.name) {
+            // a should come before b in the sorted order
+            return -1;
+        } else if (a.name > b.name) {
+            // a should come after b in sorted order
+            return 1;
+        } else {
+            // a and b are the same
+            return 0;
+        }
+    });
+    generateList();
+}); // sort string from a to z(ascending order)
+
+const button4 = document.querySelector(".sort-btn4");
+button4.addEventListener("click", () => {
+    courses.sort((a, b) => {
+        // a should come before b in the sorted order
+        if (a.name < b.name) {
+            return 1;
+            // a should come after b in the sorted order
+        } else if (a.name > b.name) {
+            return -1;
+            // a and b are the same
+        } else {
+            return 0;
+        }
+    });
+    generateList();
+}); // sort string from z to a(descending order)
+
+/*
+1)The sort() method sorts the elements of an array in place and returns the sorted array.
+2)The default sort order is ascending, built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values.
+3)way of calling sort functions
+// Functionless
+array.sort()
+
+// Arrow function
+array.sort((firstEl, secondEl) => { ... } )
+
+// Compare function
+array.sort(compareFn)
+
+// Inline compare function
+array.sort(function compareFn(firstEl, secondEl) { ... })
+4) if you use functionless sort then array elements are converted to strings, then sorted according to each 
+character's Unicode point value
+5) to compare numbers, compare function can subtract b from a. the function will sort the array in ascending order.
+example:
+    var numbers = [4, 2, 5, 1, 3];
+    numbers.sort(function(a, b) {
+    return a - b;
+    });
+    console.log(numbers); 
+    // [1, 2, 3, 4, 5] 
+    // if you want descending order then subtract a from b
+// we can write above example in shorter syntax
+    let numbers = [4, 2, 5, 1, 3];
+    numbers.sort((a, b) => a - b);
+    console.log(numbers);
+6) for more information of array sorting read "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort"
+*/
